622a7f14d55463b7d64030d48091c4dcbf6d5474
kg <864921462@qq.com>
2022-07-21 21:23:20 +0800

    2022/07/21

---
+++ 20.go
@@ -0,0 +1,22 @@
+package leetcode
+
+/*
+个人思路：栈，这题没啥意思，leetcode记录上我还做过。不过go的容器不完善，不导包没有队列。我临时没有想到好的替代栈的方案。
+答案：用一个数组暂存，手动实现栈的push和pop
+*/
+
+func isValid(s string) bool {
+	stack := make([]rune, 0)
+	for _, v := range s {
+		if v == '{' || v == '[' || v == '(' {
+			stack = append(stack, v)
+		} else if len(stack) > 0 && ((v == '}' && stack[len(stack)-1] == '{') ||
+			(v == ']' && stack[len(stack)-1] == '[') ||
+			(v == ')' && stack[len(stack)-1] == '(')) {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
---
+++ 28.go
@@ -0,0 +1,19 @@
+package leetcode
+
+/*
+个人思路：遍历，一个个比较，注意下corner case即可
+答案：霜神是两个for遍历haystack和needle，不过写的很优雅，再次折服
+*/
+
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if needle == haystack[i:i+len(needle)] {
+			return i
+		}
+	}
+	return -1
+}
