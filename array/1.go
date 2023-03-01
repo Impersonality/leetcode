e3c970e74f80b8c80f87db8bcc1cc579dbba3f0b
ljc <864921462@qq.com>
2023-03-01 11:50:54 +0800

    2022/03/01

---
+++ array/1.go
@@ -0,0 +1,17 @@
+package leetcode
+
+/*
+思路：就想着双指针了，这题第一次见应该是做出来了，思维固化了
+答案：构造map来存储每个数所需要的另一半
+*/
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if val, ok := m[nums[i]]; ok {
+			return []int{val, i}
+		}
+		m[target-nums[i]] = i
+	}
+	return []int{}
+}
