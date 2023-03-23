54c7a3e60fc953fe5cd08b850cff6d3d5291ef7a
ljc <864921462@qq.com>
2023-03-23 09:24:07 +0800

    2022/03/23

---
+++ 704.go
@@ -0,0 +1,20 @@
+package leetcode
+
+// 思路：经典二分查找
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if target < nums[mid] {
+			r = mid - 1
+		}
+		if nums[mid] < target {
+			l = mid + 1
+		}
+	}
+	return -1
+}
