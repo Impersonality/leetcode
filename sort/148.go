ca90909633ecaaa37d7a3018dc89fe36064c95d1
ljc <864921462@qq.com>
2023-03-27 08:43:26 +0800

    2022/03/24

--- sort/148.go
+++ sort/148.go
@@ -6,6 +6,8 @@ ListNode排序，147是指定用insertion，链表更适合mergeSort
 常规mergeSort使用递归，合并ListNode就是21题，
 递归会增加栈空间，空间复杂度log(n)，官答介绍了自底向上的mergeSor，就是遍历，从1开始两两Node merge
 看了下实现，理解很困难，先暂时放弃
+
+2刷，忘了，没写出来。抄了遍快排和自底向上的解法
 */
 
 type ListNode struct {
@@ -13,6 +15,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// //////////////////mergeSort/////////////////////
 func mergeList(left, right *ListNode) *ListNode {
 	if left == nil {
 		return right
@@ -29,7 +32,7 @@ func mergeList(left, right *ListNode) *ListNode {
 	}
 }
 
-func sortList(head *ListNode) *ListNode {
+func sortList0(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
@@ -40,5 +43,36 @@ func sortList(head *ListNode) *ListNode {
 	}
 	midTemp := mid.Next
 	mid.Next = nil
-	return mergeList(sortList(left), sortList(midTemp))
+	return mergeList(sortList0(left), sortList0(midTemp))
+}
+
+// //////////////////quickSort/////////////////////
+func sortList1(head *ListNode) *ListNode {
+	pre := &ListNode{Next: head}
+	quickSort(pre, nil)
+	return pre.Next
+}
+
+func quickSort(pre, tail *ListNode) {
+	if pre == tail || pre.Next == tail || pre.Next.Next == tail {
+		return
+	}
+	pivot := pre.Next
+	l := &ListNode{}
+	p1 := pre.Next
+	p2 := l
+	for p1.Next != tail {
+		if p1.Next.Val < pivot.Val {
+			p2.Next = p1.Next
+			p2 = p2.Next
+			p1.Next = p1.Next.Next
+			p2.Next = nil
+		} else {
+			p1 = p1.Next
+		}
+	}
+	p2.Next = pre.Next
+	pre.Next = l.Next
+	quickSort(pre, pivot)
+	quickSort(pivot, tail)
 }
